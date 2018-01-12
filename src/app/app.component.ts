cffa938344bf9857118c58d7781aaf0a606618ed
Mauricio Guzmán <mauricio.guzman.salazar@gmail.com>
2018-01-12 17:33:15 -0500

    currency pipe pending

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = "Mauricio";
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI = Math.PI;
+  a = 0.234;
 }
